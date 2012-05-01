b85828f0faa5371c235e15d3747f6d51cc68717d
kraki5525 <kraki5525@gmail.com>
2012-05-01 15:39:37 -0400

    Initial commit

---
+++ SpeakingNumbers/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SpeakingNumbers.Models;
+
+namespace SpeakingNumbers.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new InputModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(InputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var parser = new InputParser();
+                using (var stream = parser.Parse(model))
+                {
+                    var result = new FileStreamResult(stream, "audio/wav");
+                    result.FileDownloadName = model.NumberAsString + ".wav";
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ SpeakingNumbers/Models/InputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpeakingNumbers.Models
+{
+    public class InputModel
+    {
+        public InputModel()
+        {
+            NumberAsString = "";
+        }
+
+        [Required(ErrorMessage = "You must enter a number.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "You can only enter a valid natural number.")]
+        public string NumberAsString { get; set; } 
+    }
+}
